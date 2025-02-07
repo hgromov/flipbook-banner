65fa3bc7ac81ea97df9fecd39fd0f1fab4b474bb
Herman <hgrom9@gmail.com>
2025-02-06 23:53:28 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FlipbookBanner from "./FlipbookBanner/FlipbookBanner";
+import './app.css'
+
+function App() {
+  return (
+    <div className="App">
+      <FlipbookBanner />
+    </div>
+  );
+}
+
+export default App;
